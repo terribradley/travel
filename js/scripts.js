5a96604a90b8496cfbaebf297166cba596621f7b
Terri Bradley <TerriBradley@terri-bradleys-macbook-2.local>
2016-06-03 11:01:38 -0700

    some logical errors in js are fixed

--- js/scripts.js
+++ js/scripts.js
@@ -10,20 +10,45 @@ $(document).ready(function() {
       $("#france").hide();
       $("#morocco").hide();
 
-  if (age >= 35 && beverage === "Juice" && rain === "1") {
-    alert("hi")
-    $("#honolulu").show();
-  }
 
-  // else if (age >= 35 && beverage === "Coffee" && rain === "2") {
-  //   alert("yo");
-  //   $("#france").slideIn();
-
- else (age < 40 && beverage === "Milk" && movie === "b"){
-   alert("Hello");
-   $("#france").show();
+  if ( age >= 31 && beverage === "Coffee" && movie === "c" ) {
+    alert("yo");
+    $("#france").show();
  }
+ else if ( age <= 30 && beverage === "Coffee" && movie != "c" ) {
+   alert("hi")
+   $("#morocco").show();
+ }
+ else if (age >= 31 && beverage != "Coffee" && movie != "d" ) {
+    alert("Whats up")
+    $("#honolulu").show();
+}
+else if (age <= 30 && beverage != "Coffee" && movie!= "d") {
+  alert("Whats 2")
+  $("#honolulu").show();
+}
 
-
+ //   else if ( age <= 30 && beverage === "Tea" || "Coffee" && rain === "1" || "3" && movie === "a" || "b" ) {
+ //    alert("go");
+ //    $("#morocco").show();
+ //  }
+ //
+ //  else if ( age >= 31 && beverage != ("Tea" || "Coffee") && rain === ("1" || "2") && movie === ("a" || "c") ) {
+ //    alert("hi");
+ //    $("#honolulu").show();
+ //  }
+ //  else if ( age < 40 && beverage === "Milk" && movie === "b" ) {
+ //   alert("Hello");
+ //   $("#france").show();
+ // }
+ // // // else if ( age < 40 && beverage === "Tea" && rain === "3" && movie !== "c" ) {
+ // // //   alert("what's up!");
+ // // //   $("#morocco").show();
+ // //
+ // //
+ // if ( age >= 35 && beverage === "Juice" && rain === "1" ) {
+ //   alert("hi");
+ //   $("#honolulu").show();
+ // // }
 });
 });
