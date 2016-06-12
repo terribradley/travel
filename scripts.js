980c08877baa0e78e64c275201107a11d558d463
Terri Bradley <TerriBradley@terribrmacbook2.hsd1.or.comcast.net>
2016-06-12 10:56:24 -0700

    fixes js formatting

--- scripts.js
+++ scripts.js
@@ -13,32 +13,23 @@ $(document).ready(function() {
 
   if ( age >= 31 && (beverage === "Coffee" || beverage === "Tea") && (movie === "a" || movie === "b") ) {
     $("#france").show();
-  }
-  else if ( age <= 30 && (beverage === "Coffee" || beverage === "Tea") && (movie === "a" || movie === "b") ) {
+  } else if ( age <= 30 && (beverage === "Coffee" || beverage === "Tea") && (movie === "a" || movie === "b") ) {
     $("#morocco").show();
-  }
-  if ( age >= 31 && (beverage === "Coffee") && (movie === "c" || movie === "d") ) {
+  } if ( age >= 31 && (beverage === "Coffee") && (movie === "c" || movie === "d") ) {
     $("#australia").show();
-  }
-  else if ( age <= 30 && (beverage === "Coffee") && (movie === "c" || movie === "d") ) {
+  } else if ( age <= 30 && (beverage === "Coffee") && (movie === "c" || movie === "d") ) {
     $("#honolulu").show();
-  }
-  else if (age >= 31 && (beverage === "Juice" || beverage === "Mamosa") && movie != "d" ) {
+  } else if (age >= 31 && (beverage === "Juice" || beverage === "Mamosa") && movie != "d" ) {
     $("#honolulu").show();
-  }
-  else if (age <= 30 && (beverage === "Juice" || beverage === "Mamosa") && movie != "d" ) {
+  } else if (age <= 30 && (beverage === "Juice" || beverage === "Mamosa") && movie != "d" ) {
     $("#france").show();
-  }
-  else if (age >= 31 && (beverage === "Juice" || beverage === "Tea") && movie === "d" ) {
+  } else if (age >= 31 && (beverage === "Juice" || beverage === "Tea") && movie === "d" ) {
     $("#morocco").show();
-  }
-  else if (age <= 30 && (beverage === "Juice" || beverage === "Coffee") && movie === "d" ) {
+  } else if (age <= 30 && (beverage === "Juice" || beverage === "Coffee") && movie === "d" ) {
     $("#australia").show();
-  }
-  else if (age >= 31 && beverage != "Tea" && movie === "d" ) {
+  } else if (age >= 31 && beverage != "Tea" && movie === "d" ) {
     $("#australia").show();
-  }
-  else if (age <= 30 && beverage != "Juice" && movie === "d" ) {
+  } else if (age <= 30 && beverage != "Juice" && movie === "d" ) {
     $("#france").show();
   }
   });
